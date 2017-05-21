7e002782c9152b80925c60c8a4d06fb04c773fd0
Jeroen van Veen <j.veenvan@gmail.com>
2017-05-21 12:27:39 +0200

    Cleanup.

--- fuet.js
+++ fuet.js
@@ -1,110 +1,148 @@
 #!/usr/bin/env node
-const compiler = require('vue-template-compiler')
-const glob = require('glob')
-
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
+
+const compiler = require('vue-template-compiler')
+const glob = require('glob')
 const transpile = require('vue-template-es2015-compiler')
 
-let vueOptions = {
-    preserveWhitespace: false,
-}
 
-let namespace = 'window.templates'
+class Fuet {
 
-let yargs = require('yargs')
-    .option('namespace', {
-        alias: 'n',
-        describe: 'changes the default namespace.',
-        type: 'string',
-    })
-    .usage('Usage: $0 <command> [options]')
-    .example('$0 -i **/*.vue -o templates.js', 'write templates using global namespace.')
-    .epilog('Devhouse Spindle, https://wearespindle.com')
-    .alias('c', 'commonjs')
-    .describe('c', 'use commonjs format')
-    .boolean('c')
-    .default('c', false)
-    .alias('i', 'input')
-    .describe('i', 'glob pattern to vue templates.')
-    .string('i')
-    .alias('o', 'output')
-    .describe('o', 'output file to use.')
-    .string('o')
-    .alias('s', 'skip')
-    .describe('s', 'skip parts of the template path to name.')
-    .array('s')
-    .help('h')
+    constructor(options) {
+        this.options = options
+    }
 
 
-yargs.demandOption(['i'])
-yargs.argv
-const argv = yargs.parsed.argv
+    toFunction(code) {
+        return transpile(`function r(){${code}}`)
+    }
 
 
-function toFunction(code) {
-    let output = `function r(){${code}}`
-    return transpile(output)
-}
-
-
-let processFile = function(filename, templateName) {
-    return new Promise(function(resolve, reject) {
-        fs.readFile(filename, 'utf8', (err, data) => {
-            if(err) {
-                reject(err, templateName)
-                return
-            }
-            let compiled = compiler.compile(data, vueOptions)
-            if (compiled.errors.length) reject(compiled.errors, templateName)
-            let jsTemplate
-            if (argv.commonjs) {
-                jsTemplate = `module.exports.${templateName}={r:${toFunction(compiled.render)}`
-                if (compiled.staticRenderFns.length) {
-                    jsTemplate += `,s:[${compiled.staticRenderFns.map(toFunction).join(',')}]}`
-                } else jsTemplate += '}'
-            } else {
-                jsTemplate = `${namespace}.${templateName}={r:${toFunction(compiled.render)}`
-                if (compiled.staticRenderFns.length) {
-                    jsTemplate += `,s:[${compiled.staticRenderFns.map(toFunction).join(',')}]}`
-                } else jsTemplate += '}'
-            }
-            resolve({data: jsTemplate, name: templateName})
+    readFile(filename) {
+        return new Promise((resolve) => {
+            fs.readFile(filename, 'utf8', (err, data) => {
+                resolve(data)
+            })
         })
-    })
-}
+    }
 
 
-glob(argv.input, {}, async function(err, files) {
-    let fileReads = []
-    for (let file of files) {
-        let tmpName = file.replace(path.extname(file), '').replace(/\/|-/g, '_').replace('.._', '')
+    compileTarget(templateData, templateName) {
+        let compiled = compiler.compile(templateData, this.options.vue)
+        if (compiled.errors.length) reject(compiled.errors, templateName)
+        let jsTemplate = ''
+        if (this.options.commonjs) {
+            jsTemplate = `module.exports.${templateName}={r:${this.toFunction(compiled.render)}`
+            if (compiled.staticRenderFns.length) {
+                jsTemplate += `,s:[${compiled.staticRenderFns.map(this.toFunction).join(',')}]}`
+            } else jsTemplate += '}'
+        } else {
+            jsTemplate = `${this.options.namespace}.${templateName}={r:${this.toFunction(compiled.render)}`
+            if (compiled.staticRenderFns.length) {
+                jsTemplate += `,s:[${compiled.staticRenderFns.map(this.toFunction).join(',')}]}`
+            } else jsTemplate += '}'
+        }
+        return {data: jsTemplate, name: templateName}
+    }
+
+
+    fileToTemplateName(filename) {
+        let tmpName = filename.replace('../', '').replace(path.extname(filename), '').replace(/\/|-/g, '_')
         let templateNameParts = []
-        for (part of tmpName.split('_')) {
-            if (!argv.skip.includes(part)) {
+        for (let part of tmpName.split('_')) {
+            if (!this.options.pathfilter.includes(part)) {
                 templateNameParts.push(part)
             }
         }
         let templateName = templateNameParts.join('_')
-        fileReads.push(processFile(file, templateName))
+        return templateName
     }
-    try {
-        results = await Promise.all(fileReads)
-        let componentData = ''
-        for (let result of results) {
-            componentData += result.data
-        }
 
-        if (!argv.output) {
-            console.log(componentData)
-        } else {
-            fs.writeFile(argv.output, componentData, (err) => {
-                if(err) return console.log(err)
+
+    writeTemplateData(target, data) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(target, data, (err) => {
+                if(err) reject(err)
+                else resolve(data)
             })
-        }
-    } catch(err) {
-        console.log(err)
+        })
     }
 
-})
+
+    async processFiles(globPattern) {
+        glob(globPattern, {}, async (err, filenames) => {
+            let fileReads = filenames.map((filename) => this.readFile(filename))
+            let data = ''
+            const readFiles = await Promise.all(fileReads)
+            filenames.forEach((filename, i) => {
+                let result = this.compileTarget(readFiles[i], this.fileToTemplateName(filename))
+                data += result.data
+            })
+
+            if (!this.options.output) console.log(data)
+            else this.writeTemplateData(this.options.output, data)
+        })
+    }
+
+
+    async processFile(content, filename) {
+        return this.compileTarget(content, this.fileToTemplateName(filename))
+    }
+}
+
+
+// Only called from the cli.
+if(require.main === module) {
+    let yargs = require('yargs')
+        .usage('Usage: $0 [options]')
+        .example(`$0 -i '**/*.vue' -o templates.js`)
+        .epilog('Devhouse Spindle, https://wearespindle.com')
+        .option('commonjs', {
+            alias: 'c',
+            describe: 'use commonjs format',
+            type: 'boolean',
+            default: false,
+        })
+        .option('input', {
+            alias: 'i',
+            describe: 'glob to vue templates',
+            type: 'string',
+        })
+        .option('namespace', {
+            alias: 'n',
+            describe: 'changes namespace',
+            default: 'window.templates',
+            type: 'string',
+        })
+        .option('output', {
+            alias: 'o',
+            describe: 'output file location',
+            type: 'string',
+        })
+        .option('pathfilter', {
+            alias: 'p',
+            describe: 'template path to name filter',
+            type: 'array',
+            default: [],
+        })
+        .help('h')
+
+        yargs.demandOption(['i'])
+        yargs.detectLocale(false)
+        yargs.argv
+        const argv = yargs.parsed.argv
+        let options = {
+            pathfilter: argv.pathfilter,
+            namespace: argv.namespace || 'window.templates',
+            vue: {
+                preserveWhitespace: false,
+            }
+        }
+        if (argv.output) options.output = argv.output
+        const fuet = new Fuet(options)
+        fuet.processFiles(argv.input)
+}
+
+module.exports = Fuet
